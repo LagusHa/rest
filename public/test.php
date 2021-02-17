f9fa500222cda80e62f4a481fa4233d0e396df06
“Valery <glypw@mail.ru>
2021-02-17 18:53:54 +0300

    upgrade to php 8

---
+++ public/test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gotohell
+ * Date: 2/13/21
+ * Time: 9:20 PM
+ */
---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gotohell
+ * Date: 2/10/21
+ * Time: 9:58 PM
+ */
+
+namespace App\EventSubscriber;
+
+
+class ExceptionSubscriber
+{
+
+}
